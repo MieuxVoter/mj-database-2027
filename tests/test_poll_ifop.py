f7b5ec68606117b594da800af2f5334f9fd23ffa
ipuch <pierre.puchaud@inria.fr>
2025-12-09 23:08:30 +0100

    tests( same number of intention mention consistency)

--- tests/test_poll_ifop.py
+++ tests/test_poll_ifop.py
@@ -26,9 +26,6 @@ def test_pt3_all_sums():
         for _, row in poll_df.iterrows():
             poll_error_message = f"Dans sondage {poll.name} : pour {row.loc['candidate_id']},"
 
-            col = "intention_mention_7"
-            assert row.loc[col] == 0, f"{poll_error_message} la colonne {col} doit toujours être égale à 0"
-
             sum = 0
 
             for i in range(1, 7):
